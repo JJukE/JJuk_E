89a13ffbc21d9ed9e9e4bfe9ee0ea16d973b9649
JJukE <psj9156@naver.com>
2023-05-11 21:26:52 +0900

    cloned by https://github.com/Kitsunetic/kitsu

---
+++ JJuk_E/__init__.py
@@ -0,0 +1,3 @@
+from JJuk_E import logger, metrics, module, options, sched, trainer, utils
+
+# from jjuke import *
---
+++ setup.py
@@ -0,0 +1,23 @@
+# from setuptools import find_packages, setup
+
+# setup(
+#     name="kitsu",
+#     version="0.0.3.3",
+#     description="",
+#     author="Kitsunetic",
+#     author_email="jh.shim.gg@gmail.com",
+#     url="https://github.com/Kitsunetic/kitsu.git",
+#     packages=find_packages(),
+#     zip_safe=False,
+#     install_requires=[
+#         "torch",
+#         "numpy",
+#         "scikit-image",
+#         "point_cloud_utils",
+#         "PyMCubes",
+#         "omegaconf",
+#         "easydict",
+#         "tqdm",
+#     ],
+#     entry_points={"console_scripts": ["kitsu=kitsu.main:main"]},
+# )
