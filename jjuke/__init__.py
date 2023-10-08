43a67aebe769d3be97ff7be64799ba5cd60e1ba3
JJukE <psj9156@naver.com>
2023-10-08 21:01:22 +0900

    Add diffusion and unet modules (jjuke==0.0.3.0)

--- jjuke/__init__.py
+++ jjuke/__init__.py
@@ -1,5 +1,5 @@
-from jjuke import logger, metrics, module, options, sched, trainer, utils
+from jjuke import base_modules, metrics, utils, pointcloud, logger, sched
 
 # from jjuke import *
 
-__version__ = '0.0.1.0'
+__version__ = '0.0.3.0'
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="jjuke",
-    version="0.0.1.0",
-    description="utilities for AI models with Pytorch by JJukE",
+    version="0.0.3.0",
+    description="Modules and utilities for Deep Learning training or inference with Pytorch by JJukE",
     author="JJukE",
     author_email="psj9156@gmail.com",
     url="https://github.com/JJukE/JJuk_E.git",
@@ -12,16 +12,18 @@ setup(
     install_requires=[
         "torch",
         "numpy",
+        "pytorch3d",
         "trimesh",
-        "open3d==0.9.0.0",
-        "open3d-python==0.7.0.0",
+        # "open3d",
+        # "open3d-python==0.7.0.0",
         "scikit-image",
         "point_cloud_utils",
         "PyMCubes",
         "omegaconf",
         "easydict",
         "tqdm",
-        "timm"
+        "timm",
+        "einops==0.6.1"
     ],
     keywords=["JJukE", "jjuke"],
     entry_points={"console_scripts": ["JJukE=jjuke.main:main"]},
